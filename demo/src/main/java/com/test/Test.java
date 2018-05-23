b6fd5fa8ba102694479cbc8a4796f894bfa2ff5f
HYTHuangYunTao <1378240890@qq.com>
2018-05-23 16:37:45 +0800

    Initial commit

---
+++ demo/src/main/java/com/test/Test.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Test {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public static void main(String[] agr){
+        Scanner input=new Scanner(System.in);
+        System.out.print(input.nextInt());
+    }
+}
